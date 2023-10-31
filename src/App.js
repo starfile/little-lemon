1b804dcd1cd8df390e6b893e8b07e44006fbef51
bk <bk@bluewin.ch>
2023-10-31 21:51:36 +0100

    Some commit message of your choice goes here

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+{/*import logo from './logo.svg';*/}
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+{/*
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+*/}
+      Homepage
     </div>
   );
 }
